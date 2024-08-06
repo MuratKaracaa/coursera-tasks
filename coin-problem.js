3600b6b42efdb54c065fe90b07020ae3578e6edb
Murat Karaca <murat.karaca@commencis.com>
2024-08-07 00:43:28 +0300

    chore: add coin solution

---
+++ coin-problem.js
@@ -0,0 +1,29 @@
+const generateCoins = (amount) => {
+    if (amount === 24) {
+        return [5, 5, 7, 7]
+    } else if (amount === 25) {
+        return [5, 5, 5, 5, 5]
+    } else if (amount === 26) {
+        return [5, 7, , 7, 7]
+    } else if (amount === 27) {
+        return [7, 7, 5, 5]
+    } else if (amount === 28) {
+        return [7, 7, 7, 7]
+    }
+
+    const coins = generateCoins(amount - 5)
+    coins.push(5)
+    return coins
+}
+
+const findMax = (n) => {
+    const arr = generateCoins(n)
+    console.log(n)
+    if (arr.length === 0) {
+        return n
+    } else {
+        return findMax(n + 1)
+    }
+}
+
+console.log(findMax(24))
