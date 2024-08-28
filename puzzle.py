3514c54d6643a5c21e3eec83b5002f43e6c3a465
Murat Karaca <murat.karaca@commencis.com>
2024-08-28 23:18:19 +0300

    chore: format

--- puzzle.py
+++ puzzle.py
@@ -1,16 +1,18 @@
-import math
 import heapq
+import math
 
 goal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
 
+
 def is_within_range(number, n):
-    return 0 <= number <= n*n - 1
+    return 0 <= number <= n * n - 1
+
 
 def get_manhattan_distance(current, target):
     sum = 0
     n = len(target)
 
-    for i in range(0,n):
+    for i in range(0, n):
         if current[i] != 0:
             current_row = math.floor(i / 4)
             current_column = i % 4
@@ -23,7 +25,7 @@ def get_manhattan_distance(current, target):
 
 
 class Node:
-    def __init__(self, current_state, parent_node,current_step ,total_cost, switcher_index):
+    def __init__(self, current_state, parent_node, current_step, total_cost, switcher_index):
         self.current_state = current_state
         self.parent_node = parent_node
         self.current_step = current_step
@@ -37,14 +39,13 @@ class Node:
         return self.current_state == other.current_state
 
     def __gt__(self, other):
-            return self.total_cost > other.total_cost
+        return self.total_cost > other.total_cost
 
     def __hash__(self):
         return hash(tuple(self.current_state))
 
     def __repr__(self):
         return f"Node(current_state={self.current_state}, current_step={self.current_step}, parent_state={self.parent_node}, total_cost={self.total_cost}, switcher_index={self.switcher_index})"
-
 
 
 def generate_states(current, n):
@@ -59,18 +60,17 @@ def generate_states(current, n):
     is_left_edge = False
     is_right_edge = False
 
-
     if i == 0:
         is_top_left_corner = True
     if i == n * n - n:
         is_bottom_left_corner = True
     if i == n - 1:
         is_top_right_corner = True
-    if i == n*n - 1:
+    if i == n * n - 1:
         is_bottom_right_corner = True
     if i % n == 0:
         is_left_edge = True
-    if i % n == n-1:
+    if i % n == n - 1:
         is_right_edge = True
 
     if is_top_left_corner:
@@ -78,13 +78,13 @@ def generate_states(current, n):
     elif is_bottom_left_corner:
         possible_indexes = [i + 1, i - n]
     elif is_top_right_corner:
-        possible_indexes = [i-1, i+n]
+        possible_indexes = [i - 1, i + n]
     elif is_bottom_right_corner:
-        possible_indexes = [i-1, i-n]
+        possible_indexes = [i - 1, i - n]
     elif is_left_edge:
         possible_indexes = [i + 1, i - n, i + n]
     elif is_right_edge:
-        possible_indexes = [i-1, i-n, i+n]
+        possible_indexes = [i - 1, i - n, i + n]
     else:
         possible_indexes = list(filter(lambda number: is_within_range(number, n), [i + 1, i - 1, i - n, i + n]))
 
@@ -95,12 +95,11 @@ def generate_states(current, n):
         temp_state[pos] = temp
         obj = {
             'state': temp_state,
-            'switcher_index' : pos
+            'switcher_index': pos
         }
         result.append(obj)
 
     return result
-
 
 
 def get_a_star_result(start, goal):
@@ -116,7 +115,6 @@ def get_a_star_result(start, goal):
         next_node = heapq.heappop(queue)
         next_state = next_node.current_state
 
-
         if next_state == goal:
             print("done")
             return construct_backwards(next_node, [])
@@ -127,19 +125,23 @@ def get_a_star_result(start, goal):
             if tuple(obj['state']) not in check_set:
                 check_set.add(tuple(obj['state']))
                 distance = get_manhattan_distance(obj['state'], goal)
-                new_node = Node(obj['state'], next_node, next_node.current_step + 1, next_node.current_step + 1 + distance, obj['switcher_index'])
+                new_node = Node(obj['state'], next_node, next_node.current_step + 1,
+                                next_node.current_step + 1 + distance, obj['switcher_index'])
                 heapq.heappush(queue, new_node)
 
     return None
 
+
 def construct_backwards(node, final_list):
-    if(node.current_step == 0):
+    if node.current_step == 0:
         return final_list
 
     final_list.insert(0, node.switcher_index)
     return construct_backwards(node.parent_node, final_list)
 
+
 def solution(position):
     return get_a_star_result(position, goal)
 
-print(solution([5,1,4,8,9,6,3,11,10,2,15,7,13,14,12,0]))
+
+print(solution([5, 1, 4, 8, 9, 6, 3, 11, 10, 2, 15, 7, 13, 14, 12, 0]))
